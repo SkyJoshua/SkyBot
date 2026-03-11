7844b21160373035c15821631ec703dc90fa9c33
skyjoshua <mcjoshuagaming@outlook.com>
2026-03-11 04:50:07 +0000

    updated cmds/help

--- Program.cs
+++ Program.cs
@@ -150,10 +150,10 @@ client.MessageService.MessageReceived += async (message) =>
                                                                 - `s/joincode - Sends a link to a github that you can use to make your bot join your planet.`
                                                                 - `s/joinsite - Sends a link to a website that you can use to make yout bot join your planet.`
                                                                 - `s/api|swagger - Sends a link to the Swagger API`
-                                                                - `s/cmds - Shows this list`
+                                                                - `s/cmds|help - Shows this list`
                                                                 - `s/usercount - Shows the user count of Valour`
                                                                 - `s/devcentral - Sends the invite link to the Dev Central Planet`
-                                                                - `s/mc - Sends ValourSMP IP`
+                                                                - `s/mc - Sends Unofficial ValourSMP IP`
                                                                 ");
     };
 
